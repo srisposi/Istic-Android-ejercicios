798f8aad700d16d818d819c9d4525c0aa89e7c3d
srisposi <srisposi@iiconcepcion.edu.ar>
2020-05-20 04:57:06 -0300

    sigue 3

--- MyApplication/app/src/main/java/com/example/myapplication/registro.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/registro.kt
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_quien_soy.*
 import kotlinx.android.synthetic.main.activity_registro.*
+import kotlinx.android.synthetic.main.activity_registro.lblLegajoQuienSoy
 
 class registro : AppCompatActivity() {
 
@@ -16,11 +18,20 @@ class registro : AppCompatActivity() {
         //var actividad = Intent(this,registro::class.java)
         //startActivity(actividad)
 
-    btnRegistro.setOnClickListener{
-        val volver2:Intent = Intent(this,MainActivity::class.java)
-        Toast.makeText(this,"REGISTRO CORRECTO",Toast.LENGTH_LONG).show()
-        startActivity(volver2)
-    }
+        btnRegistro.setOnClickListener{
+            val volver2:Intent = Intent(this,MainActivity::class.java)
+            Toast.makeText(this,"REGISTRO CORRECTO",Toast.LENGTH_LONG).show()
+            startActivity(volver2)
+        }
+
+        btnQuienSoy.setOnClickListener {
+            lblNombreQuienSoy.text.toString()="SERGIO DANIEL"
+            lblApellidoQuienSoy.text.toString()="RISPOSI"
+            lblLegajoQuienSoy.text.toString()="32411"
+            lblGitHubQuienSoy.text.toString()="https://github.com/srisposi/Istic-Android-ejercicios"
+            val actividadQuienSoy:Intent = Intent(this,quienSoy::class.java)
+            startActivity(actividadQuienSoy)
+        }
 
     }
 }
