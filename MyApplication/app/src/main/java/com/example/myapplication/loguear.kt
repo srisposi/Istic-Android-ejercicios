55ee9ce75d9b5539eec1b0d53d67d816cb786bdb
srisposi <srisposi@iiconcepcion.edu.ar>
2020-05-19 22:18:46 -0300

    AL FIN PUEDO SUBIR DE NUEVO A GIT HUBgit add .git add . PROBLEMA INDEX.LOCK RESUELTO

---
+++ MyApplication/app/src/main/java/com/example/myapplication/loguear.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_loguear.*
+
+class activity_loguear : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnVolver.setOnClickListener{
+            val volver = Intent(this,loguear::class.java)
+            startActivity(volver)
+        }
+
+    }
+}
