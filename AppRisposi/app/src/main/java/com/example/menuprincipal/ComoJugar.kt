3719edfa29f72a5e3698000533de2621864c3027
srisposi <srisposi@iiconcepcion.edu.ar>
2020-06-28 21:22:29 -0300

    Terminando

--- AppRisposi/app/src/main/java/com/example/menuprincipal/ComoJugar.kt
+++ AppRisposi/app/src/main/java/com/example/menuprincipal/ComoJugar.kt
@@ -1,12 +1,33 @@
 package com.example.menuprincipal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_como_jugar.*
+import kotlinx.android.synthetic.main.activity_screen_ayuda.*
 
 class ComoJugar : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_como_jugar)
+
+
+        val btnVolverAyudaJugar=findViewById<Button>(R.id.btnVolverAyudaJugar)
+        btnVolverAyudaJugar.setOnClickListener {
+            try {
+                val volverAyudaJugar = Intent (this, AdivinarNumero::class.java)
+                startActivity(volverAyudaJugar)
+
+            }catch (e: Throwable){
+                Toast.makeText(this, "Se produjo un error en la ayuda, Pruebe de nuevo" + e.message, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+
     }
 }
